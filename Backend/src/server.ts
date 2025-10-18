52390fda63efdc5bbf87e96c67269b7a927e3457
Kavya-76 <kavyabansal2000@gmail.com>
2025-10-18 19:17:52 +0530

    Basic backend

---
+++ Backend/src/server.ts
@@ -0,0 +1,22 @@
+import express from "express"
+import connectDB from "./config/db.js"
+import cors from "cors"
+import dotenv from "dotenv"
+dotenv.config();
+
+const app = express()
+
+app.use(express.json())
+app.use(cors());
+
+connectDB();
+
+app.get("/", (req,res)=>{
+    res.send("Server is running...")
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port: ${PORT}`);
+})
